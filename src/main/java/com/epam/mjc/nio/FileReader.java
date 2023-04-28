bf3820aeb8a2aa81c2d039dd6fc0bab754a2d51e
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-28 23:31:40 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        StringBuilder stringBuilder = new StringBuilder();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                char ch = (char) c;
+                stringBuilder.append(ch);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
